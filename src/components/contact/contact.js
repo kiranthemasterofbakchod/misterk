9a5cd4bafe0a1623d6d6b399ded21b7a4589e888
unknown <kirankumargs23.kkgs@gmail.com>
2020-11-12 19:23:24 +0530

    projects updated

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -3,67 +3,64 @@ import React, { useCallback, useRef } from 'react'
 import './contact.css'
 
 const Contact = () => {
-  const observer = useRef(
-    new IntersectionObserver(
-      (entries) => {
-        if (entries[0].isIntersecting) {
-          console.log(entries[0].target)
-          entries[0].target.classList.add('anim')
-          entries[0].target.classList.remove('out')
-        } else {
-          entries[0].target.classList.remove('anim')
-          entries[0].target.classList.add('out')
-        }
-      },
-      {
-        rootMargin: '0px 0px -300px 0px',
-      }
-    )
-  )
-  const contact = useCallback((node) => {
-    if (observer.current) observer.current.observe(node)
-  })
-  return (
-    <div className='contact'>
-      <div ref={contact} className='skills-container'>
-        <div className='sect-a'>
-          <span className='skill-title'>FRONT-END</span>
-          <ul className='skills'>
-            <li className='skill'>HTML</li>
-            <li className='skill'>CSS</li>
-            <li className='skill'>JAVASCRIPT</li>
-          </ul>
-        </div>
-        <div className='frame'>
-          <span className='skill-title'>FAMEWORKS</span>
-          <ul className='skills'>
-            <li className='skill'>REACT</li>
-            <li className='skill'>SASS</li>
-            <li className='skill'>EXPRESS.JS</li>
-          </ul>
-        </div>
-        <div className='sect-b'>
-          <span className='skill-title'> BACK-END</span>
-          <ul className='skills'>
-            <li className='skill'>NODE.JS</li>
-            <li className='skill'>MONGODB</li>
-            <li className='skill'>PASSPORT.JS</li>
-          </ul>
-        </div>
-      </div>
-      <div className='git-container'>
-        <div className='git-content'>
-          <a
-            target='_blank'
-            href='https://github.com/thekiran'
-            className='contact-btn'>
-            <i className='fab fa-github fa-2x'></i>
-            {/* Github */}
-          </a>
-        </div>
-      </div>
-    </div>
-  )
+	const observer = useRef(
+		new IntersectionObserver(
+			(entries) => {
+				if (entries[0].isIntersecting) {
+					console.log(entries[0].target)
+					entries[0].target.classList.add('anim')
+					entries[0].target.classList.remove('out')
+				} else {
+					entries[0].target.classList.remove('anim')
+					entries[0].target.classList.add('out')
+				}
+			},
+			{
+				rootMargin: '0px 0px -300px 0px'
+			}
+		)
+	)
+	const contact = useCallback((node) => {
+		if (observer.current) observer.current.observe(node)
+	})
+	return (
+		<div className='contact'>
+			<div ref={contact} className='skills-container'>
+				<div className='sect-a'>
+					<span className='skill-title'>FRONT-END</span>
+					<ul className='skills'>
+						<li className='skill'>HTML</li>
+						<li className='skill'>CSS</li>
+						<li className='skill'>JAVASCRIPT</li>
+					</ul>
+				</div>
+				<div className='frame'>
+					<span className='skill-title'>FRAMEWORKS</span>
+					<ul className='skills'>
+						<li className='skill'>REACT</li>
+						<li className='skill'>SASS</li>
+						<li className='skill'>EXPRESS.JS</li>
+					</ul>
+				</div>
+				<div className='sect-b'>
+					<span className='skill-title'> BACK-END</span>
+					<ul className='skills'>
+						<li className='skill'>NODE.JS</li>
+						<li className='skill'>MONGODB</li>
+						<li className='skill'>PASSPORT.JS</li>
+					</ul>
+				</div>
+			</div>
+			<div className='git-container'>
+				<div className='git-content'>
+					<a target='_blank' href='https://github.com/thekiran' className='contact-btn'>
+						<i className='fab fa-github fa-2x' />
+						{/* Github */}
+					</a>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Contact
--- src/components/showcase/showcase.js
+++ src/components/showcase/showcase.js
@@ -3,84 +3,88 @@ import Project from './project'
 
 import photo2 from '../../img/photo2.jpg'
 import hotel from '../../img/photo1.png'
+import shop from '../../img/ecom.png'
+import transfer from '../../img/transfer.png'
 
 import './showcase.css'
 
 const data = [
-  {
-    content: <p>Hotel WebSite</p>,
-    src: hotel,
-    codeStatus: true,
-    url: 'https://thekiran.github.io/hotelsite/',
-    repo: 'https://github.com/thekiran/hotelsite',
-  },
-  {
-    content: <p>Weather App</p>,
-    src: photo2,
-    codeStatus: true,
-    url: 'https://weatherappkiran.herokuapp.com/',
-    repo: 'https://github.com/thekiran/weather-app',
-  },
-  // {
-  //   content: <p>more details gagi click madi</p>,
-  //   src: '',
-  //   codeStatus: true,
-  //   url: '',
-  // },
-  // {
-  //   content: <p>more details gagi click madi</p>,
-  //   src: '',
-  //   codeStatus: true,
-  //   url: '',
-  // },
+	{
+		content: <p>Ecommerce Site</p>,
+		src: shop,
+		codeStatus: true,
+		url: 'https://ecommercekiran.herokuapp.com/',
+		repo: 'https://github.com/thekiran/ecommerce'
+	},
+	{
+		content: <p>Weather App</p>,
+		src: photo2,
+		codeStatus: true,
+		url: 'https://weatherappkiran.herokuapp.com/',
+		repo: 'https://github.com/thekiran/weather-app'
+	},
+	{
+		content: <p>Money Transfer Site</p>,
+		src: transfer,
+		codeStatus: true,
+		url: 'https://thekiran.github.io/money/',
+		repo: 'https://github.com/thekiran/money'
+	},
+	{
+		content: <p>Hotel WebSite</p>,
+		src: hotel,
+		codeStatus: true,
+		url: 'https://thekiran.github.io/hotelsite/',
+		repo: 'https://github.com/thekiran/hotelsite'
+	}
 ]
 const Showcase = () => {
-  // const options = {
-  // 	rootMargin: '0px 0px -150px 0px'
-  // };
-  // const observer = useRef(
-  // 	new IntersectionObserver((entry) => {
-  // 		// console.log(entry[0].isIntersecting);
-  // 		if (entry[0].isIntersecting) {
-  // 			entry[0].target.classList.add('anim');
-  // 			observer.current.unobserve(entry[0].target);
-  // 		}
-  // 		// else {
-  // 		// 	entry[0].target.classList.remove('anim');
-  // 		// }
-  // 	}, options)
-  // );
+	// const options = {
+	// 	rootMargin: '0px 0px -150px 0px'
+	// };
+	// const observer = useRef(
+	// 	new IntersectionObserver((entry) => {
+	// 		// console.log(entry[0].isIntersecting);
+	// 		if (entry[0].isIntersecting) {
+	// 			entry[0].target.classList.add('anim');
+	// 			observer.current.unobserve(entry[0].target);
+	// 		}
+	// 		// else {
+	// 		// 	entry[0].target.classList.remove('anim');
+	// 		// }
+	// 	}, options)
+	// );
 
-  // const show = useCallback((node) => {
-  // 	if (observer.current) observer.current.observe(node);
-  // });
-  const show = useCallback((node) => {
-    setTimeout(() => {
-      // node.classList.add('anim')
-    }, 1000)
-  })
-  return (
-    <div className='showcase-container' ref={show}>
-      <div className='showcase-content'>
-        <div className='showcase'>
-          {data.map((ele, index, shower) => {
-            return (
-              <Project
-                key={index}
-                index={index}
-                codeStatus={ele.codeStatus}
-                content={ele.content}
-                src={ele.src}
-                shower={true}
-                url={ele.url}
-                repo={ele.repo}
-              />
-            )
-          })}
-        </div>
-      </div>
-    </div>
-  )
+	// const show = useCallback((node) => {
+	// 	if (observer.current) observer.current.observe(node);
+	// });
+	const show = useCallback((node) => {
+		setTimeout(() => {
+			// node.classList.add('anim')
+		}, 1000)
+	})
+	return (
+		<div className='showcase-container' ref={show}>
+			<div className='showcase-content'>
+				<div className='showcase'>
+					{data.map((ele, index, shower) => {
+						return (
+							<Project
+								key={index}
+								index={index}
+								codeStatus={ele.codeStatus}
+								content={ele.content}
+								src={ele.src}
+								shower={true}
+								url={ele.url}
+								repo={ele.repo}
+							/>
+						)
+					})}
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Showcase
